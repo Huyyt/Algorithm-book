0da17c858410daa7b7a663b230e05652e74a90fc
Huyyt <38221302+Huyyt@users.noreply.github.com>
2018-09-18 12:47:04 +0800

    .

---
+++ Graph/Hijsktra.cpp
@@ -0,0 +1,41 @@
+//Hijkstra
+struct HeapNode
+{
+        int d, u;
+        bool operator < (const HeapNode& rhs) const
+        {
+                return d > rhs.d;
+        }
+} zz;
+void Hijkstra(int s)
+{
+        priority_queue<HeapNode> que;
+        for (int i = 1; i <= n; i++)
+        {
+                mindist[i] = INT_MAX;
+        }
+        mindist[s] = 0;
+        mem(vis, 0);
+        zz.d = 0, zz.u = s;
+        que.push(zz);
+        while (!que.empty())
+        {
+                HeapNode x = que.top();
+                que.pop();
+                int u = x.u;
+                if (vis[u] || mindist[u] != x.d)
+                        continue
+                        vis[u] = true;
+                for (int v, i = Head[u]; i; i = nxt[i])
+                {
+                        v = to[i];
+                        if (mindist[v] > mindist[u] + cost[i])
+                        {
+                                mindist[v] = mindist[u] + cost[i];
+                                //p[v]=u;
+                                zz.d = mindist[v], zz.u = v;
+                                que.push(zz);
+                        }
+                }
+        }
+}
---
+++ Graph/Loop-tree.cpp
@@ -0,0 +1,270 @@
+//Loop-tree
+/*Huyyt*/
+#include<bits/stdc++.h>
+#define mem(a,b) memset(a,b,sizeof(a))
+#define pb push_back
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+using namespace std;
+const int MAXN = 1e6 + 5, MAXM = 1e6 + 5;
+int to[MAXM << 1], nxt[MAXM << 1], Head[MAXN], ed = 1;
+int value[MAXM << 1];
+int i, v;
+inline void addedge(int u, int v, int val)
+{
+        to[++ed] = v;
+        nxt[ed] = Head[u];
+        value[ed] = val;
+        Head[u] = ed;
+}
+inline void read(int &v)
+{
+        v = 0;
+        char c = 0;
+        int p = 1;
+        while (c < '0' || c > '9')
+        {
+                if (c == '-')
+                {
+                        p = -1;
+                }
+                c = getchar();
+        }
+        while (c >= '0' && c <= '9')
+        {
+                v = (v << 3) + (v << 1) + c - '0';
+                c = getchar();
+        }
+        v *= p;
+}
+bool circle[MAXN];
+int vis[MAXN], cnt;
+int nnxt[MAXN], nxtcircle[MAXN];
+int que[MAXN << 1];
+ll dpd[MAXN], sum[MAXN << 1], pre[MAXN];
+ll sa[MAXN], ansnow, anser;
+stack<int> sk;
+void dfs(int x, int pre)
+{
+        vis[x] = 1;
+        for (int v, i = Head[x]; i; i = nxt[i])
+        {
+                v = to[i];
+                if (pre != -1 && i == (pre ^ 1))
+                {
+                        continue;
+                }
+                if (vis[v] == 1)
+                {
+                        circle[x] = true;
+                        nxtcircle[x] = nnxt[x] = i;
+                        int cur;
+                        cur = to[nxtcircle[x]];
+                        while (cur != x)
+                        {
+                                circle[cur] = true;
+                                nxtcircle[cur] = nnxt[cur];
+                                cur = to[nxtcircle[cur]];
+                        }
+                }
+                else if (vis[v] == 2)
+                {
+                        continue;
+                }
+                else
+                {
+                        nnxt[x] = i;
+                        dfs(v, i);
+                }
+        }
+        vis[x] = 2;
+}
+struct Dstate
+{
+        int x, pre, curi;
+        Dstate(const int &X = 0, const int &P = 0, const int &C = 0): x(X), pre(P), curi(C) { }
+};
+stack<Dstate> Dst;
+void DFS(int x, int pre)
+{
+        int i, v;
+        while (Dst.size())
+        {
+                Dst.pop();
+        }
+        Dst.push(Dstate(x, pre, Head[x]));
+        while (Dst.size())
+        {
+                i = Dst.top().curi;
+                v = to[i];
+                x = Dst.top().x;
+                pre = Dst.top().pre;
+                if (!i)
+                {
+                        //exit
+                        Dst.pop();
+                        vis[x] = 2;
+                        continue;
+                }
+                else if ((pre != -1 && i == (pre ^ 1))
+                                || vis[v] == 2)
+                {
+                        //continue
+                        Dst.top().curi = nxt[i];
+                        continue;
+                }
+                if (vis[v] == 1)
+                {
+                        circle[x] = true;
+                        nxtcircle[x] = nnxt[x] = i;
+                        int cur = v;
+                        while (cur != x)
+                        {
+                                circle[cur] = true;
+                                nxtcircle[cur] = nnxt[cur];
+                                cur = to[nxtcircle[cur]];
+                        }
+                        Dst.pop();
+                }
+                else
+                {
+                        nnxt[x] = i;
+                        Dst.push(Dstate(v, i, Head[v]));
+                }
+        }
+        return;
+}
+struct Zstate
+{
+        int x, i;
+        bool usef;
+        Zstate(const int &X = 0, const int &I = 0): x(X), i(I)
+        {
+                usef = false;
+        }
+};
+stack<Zstate> Zst;
+void ZJdp(int x)
+{
+        vis[x] = 1;
+        for (int v, i = Head[x]; i; i = nxt[i])
+        {
+                v = to[i];
+                if (vis[v])
+                {
+                        continue;
+                }
+                ZJdp(v);
+                if (!circle[v])
+                {
+                        ansnow = max(ansnow, dpd[x] + dpd[v] + value[i]);
+                        dpd[x] = max(dpd[x], dpd[v] + value[i]);
+                }
+        }
+}
+void JZDP(int x)
+{
+        int i, v;
+        bool flag;
+        while (Zst.size())
+        {
+                Zst.pop();
+        }
+        Zst.push(Zstate(x, Head[x]));
+        while (Zst.size())
+        {
+                x = Zst.top().x;
+                i = Zst.top().i;
+                flag = Zst.top().usef;
+                v = to[i];
+                vis[x] = true;
+                if (!i)
+                {
+                        Zst.pop();
+                        continue;
+                }
+                if (vis[v])
+                {
+                        Zst.top().i = nxt[i];
+                        Zst.top().usef = false;
+                        continue;
+                }
+                if (!flag)
+                {
+                        Zst.top().usef = true;
+                        Zst.push(Zstate(v, Head[v]));
+                }
+                else if (!circle[v])
+                {
+                        ansnow = max(ansnow, dpd[x] + dpd[v] + value[i]);
+                        dpd[x] = max(dpd[x], dpd[v] + value[i]);
+                }
+        }
+        return;
+}
+int main()
+{
+        int n;
+        int u;
+        read(n);
+        for (i = 1; i <= n; i++)
+        {
+                read(u), read(v);
+                addedge(i, u, v), addedge(u, i, v);
+        }
+        for (i = 1; i <= n; i++)
+        {
+                if (!vis[i])
+                {
+                        DFS(i, -1);
+                        //dfs(i, -1);
+                }
+        }
+        mem(vis, 0);
+        int top, tail, et, cur;
+        for (i = 1; i <= n; i++)
+        {
+                if (circle[i])
+                {
+                        if (!vis[i])
+                        {
+                                ansnow = 0;
+                                JZDP(i);
+                                //ZJdp(i);
+                                top = tail = cnt = 0;
+                                que[tail++] = 0;
+                                cur = to[nxtcircle[i]];
+                                sa[cnt] = dpd[i];
+                                pre[cnt++] = value[nxtcircle[i]];
+                                while (cur != i)
+                                {
+                                        sa[cnt] = dpd[cur];
+                                        pre[cnt++] = value[nxtcircle[cur]];
+                                        cur = to[nxtcircle[cur]];
+                                }
+                                for (et = 1; et < (cnt << 1); et++)
+                                {
+                                        sum[et] = sum[et - 1] + pre[(et - 1) >= cnt ? (et - 1 - cnt) : (et - 1)];
+                                }
+                                for (et = 1; et < (cnt << 1); ++et)
+                                {
+                                        while (top < tail && et - que[top] >= cnt)
+                                        {
+                                                ++top;
+                                        }
+                                        u = que[top];
+                                        ansnow = max(ansnow, sa[et >= cnt ? et - cnt : et] + sa[u >= cnt ? u - cnt : u] + sum[et] - sum[u]);
+                                        while (top < tail && sa[et >= cnt ? et - cnt : et] >= sa[que[tail - 1] >= cnt ? que[tail - 1] - cnt : que[tail - 1]] + sum[et] - sum[que[tail - 1]])
+                                        {
+                                                --tail;
+                                        }
+                                        que[tail++] = et;
+                                }
+                                anser += ansnow;
+                        }
+                }
+        }
+        printf("%lld", anser);
+        return 0;
+}
