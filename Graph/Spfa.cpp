8827df3b2e6f70463532a6cb10eec6ff7af810a5
Huyyt <38221302+Huyyt@users.noreply.github.com>
2018-10-22 01:32:35 +0800

    Update Graph/Spfa.cpp

--- Graph/Spfa.cpp
+++ Graph/Spfa.cpp
@@ -13,20 +13,20 @@ int spfa(int s, int N) {
         while (q.size()) {
                 int x = q.front();
                 q.pop();
-                v[x] = 0;
+                vis[x] = 0;
                 for (int v, i = Head[x]; i; i = nxt[i]) {
                         v = to[i];
                         int c = cost[i];
                         if (d[v] > d[x] + c) {
                                 d[v] = d[x] + c;
                                 cnt[v] = cnt[x] + 1;
-                        }
-                        if (cnt[v] >= n) {
-                                return -1;
-                        }
-                        if (!vis[v]) {
-                                q.push(v);
-                                vis[v] = 1;
+                                if (cnt[v] >= N) {
+                                        return -1;
+                                }
+                                if (!vis[v]) {
+                                        q.push(v);
+                                        vis[v] = 1;
+                                }
                         }
                 }
         }
