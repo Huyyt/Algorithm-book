7a34980d11125987a86cae615adf9078c4a5182c
Huyyt <38221302+Huyyt@users.noreply.github.com>
2018-11-27 15:31:15 +0800

    Update Hijsktra.cpp

--- Graph/Hijsktra.cpp
+++ Graph/Hijsktra.cpp
@@ -5,22 +5,25 @@ struct HeapNode {
                 return d > rhs.d;
         }
 } zz;
+int mindist[MAXN];
+bool vis[MAXN];
+priority_queue<HeapNode> que;
 void Hijkstra(int s) {
-        priority_queue<HeapNode> que;
         for (int i = 1; i <= n; i++) {
                 mindist[i] = INT_MAX;
         }
         mindist[s] = 0;
-        mem(vis, 0);
+        memset(vis, 0, sizeof(vis));
         zz.d = 0, zz.u = s;
         que.push(zz);
         while (!que.empty()) {
                 HeapNode x = que.top();
                 que.pop();
                 int u = x.u;
-                if (vis[u] || mindist[u] != x.d)
-                        continue
-                        vis[u] = true;
+                if (vis[u] || mindist[u] != x.d) {
+                        continue;
+                }
+                vis[u] = true;
                 for (int v, i = Head[u]; i; i = nxt[i]) {
                         v = to[i];
                         if (mindist[v] > mindist[u] + cost[i]) {
