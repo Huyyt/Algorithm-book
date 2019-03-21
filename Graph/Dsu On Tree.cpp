7e3a649c91934cdf6c699412510429ebb5a7753a
Huyyt <38221302+Huyyt@users.noreply.github.com>
2019-03-21 17:19:45 +0800

    Update Dsu On Tree.cpp

--- Graph/Dsu On Tree.cpp
+++ Graph/Dsu On Tree.cpp
@@ -1,82 +1,38 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-
-using namespace std;
-
-const int maxn=100010;
-struct node{
-    int to,nxt;
-}t[maxn];int head[maxn],tot=0;
-inline void addedge(int x,int y){
-    t[++tot].to=y;
-    t[tot].nxt=head[x];
-    head[x]=tot;
-}
-
-int c[maxn],size[maxn],son[maxn],cnt[maxn],ans[maxn];
-//  color   size       重儿子    计数器  
-
-void dfs1(int x,int fa){
-    size[x]=1;
-    for(int i=head[x];i;i=t[i].nxt){
-        int to=t[i].to;
-        if(to!=fa){
-            dfs1(to,x);
-            size[x]+=size[to];
-            if(size[to]>size[son[x]])
-                son[x]=to;
+int n, cc[SZ], col[SZ], ks[SZ], anss[SZ];
+int sz[SZ], son[SZ];
+int skip = 0;
+void gs(int x, int f = 0) {
+        sz[x] = 1;
+        for esb(x, e, b) {
+                if (b == f) {
+                        continue;
+                }
+                gs(b, x);
+                sz[x] += sz[b];
+                if (sz[b] > sz[son[x]]) {
+                        son[x] = b;
+                }
         }
-    }
 }
-//                    重儿子    保留答案
-int dfs2(int u,int fa,int isson,int keep){
-    if(keep){
-        for(int i=head[u];i;i=t[i].nxt){
-            int to=t[i].to;
-            if(to!=fa&&to!=son[u]){
-                dfs2(to,u,0,1);
-            }
-        }
-    }
-    int tmp=0;
-    if(!keep&&son[u])tmp+=dfs2(son[u],u,1,0);
-    else if(son[u]) tmp+=dfs2(son[u],u,1,1);
-    for(int i=head[u];i;i=t[i].nxt){
-        int v=t[i].to;
-        if(v!=fa&&v!=son[u]){
-            tmp+=dfs2(v,u,0,0);
-        }
-    }
-    if(!cnt[c[u]]){
-        tmp++;
-    }
-    cnt[c[u]]++;
-    if(keep)ans[u]=tmp;
-    if(keep&&!isson) memset(cnt,0,sizeof(cnt));
-    return tmp;
+void edt(int x, int f, int v) {
+        cc[col[x]] += v;
+        for esb(x, e, b)
+                if (b != f && b != skip) {
+                        edt(b, x, v);
+                }
 }
-
-int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=1;i<n;i++){
-        int x,y;
-        scanf("%d%d",&x,&y);
-        addedge(x,y);
-        addedge(y,x);
-    }
-    for(int i=1;i<=n;i++){
-        scanf("%d",&c[i]);
-    }
-    dfs1(1,0);
-    dfs2(1,0,1,1);
-    int m;
-    scanf("%d",&m);
-    while(m--){
-        int x;
-        cin>>x;
-        cout<<ans[x]<<endl;
-    }
-    return 0;
+void dfs(int x, int f = 0, bool kep = 0) {
+        for esb(x, e, b)
+                if (b != f && b != son[x]) {
+                        dfs(b, x);
+                }
+        if (son[x]) {
+                dfs(son[x], x, 1), skip = son[x];
+        }
+        edt(x, f, 1);
+        anss[x] = cc[ks[x]];
+        skip = 0;
+        if (!kep) {
+                edt(x, f, -1);
+        }
 }
