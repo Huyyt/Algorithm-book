82bd27266e7b5beeabd4546ef08863f259e6ee3e
Huyyt <38221302+Huyyt@users.noreply.github.com>
2019-07-18 11:47:33 +0800

    Update Mcmf Large dumpling.cpp

--- Graph/Mcmf Large dumpling.cpp
+++ Graph/Mcmf Large dumpling.cpp
@@ -102,3 +102,99 @@ int main() {
         init();
         return 0;
 }
+
+
+
+
+
+
+
+
+
+// MAXMCOST
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+typedef long long ll;
+const int MAXN = 250005;
+const int MAXM = 6000005;
+struct qwe {
+    int ne, no, to, va, c;
+} e[MAXM];
+int h[MAXN], fr[MAXN], sm, cnt, ans;
+int dis[MAXM];
+bool v[MAXN];
+int s, t;
+int read()
+{
+    int r = 0, f = 1;
+    char p = getchar();
+    while (p > '9' || p < '0') {
+        if (p == '-') {
+            f = -1;
+        }
+        p = getchar();
+    }
+    while (p >= '0' && p <= '9') {
+        r = r * 10 + p - 48;
+        p = getchar();
+    }
+    return r * f;
+}
+void add(int u, int v, int w, int c)
+{
+    cnt++;
+    e[cnt].ne = h[u];
+    e[cnt].no = u;
+    e[cnt].to = v;
+    e[cnt].va = w;
+    e[cnt].c = c;
+    h[u] = cnt;
+}
+void ins(int u, int v, int w, int c)
+{
+    add(u, v, w, c);
+    add(v, u, 0, -c);
+}
+bool spfa()
+{
+    for (int i = s; i <= t; i++) {
+        dis[i] = -1e9;
+    }
+    queue<int>q;
+    q.push(s);
+    v[s] = 1;
+    dis[s] = 0;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        v[u] = 0;
+        for (int i = h[u]; i; i = e[i].ne)
+            if (e[i].va > 0 && dis[e[i].to] < dis[u] + e[i].c) {
+                dis[e[i].to] = dis[u] + e[i].c;
+                fr[e[i].to] = i;
+                if (!v[e[i].to]) {
+                    v[e[i].to] = 1;
+                    q.push(e[i].to);
+                }
+            }
+    }
+    return dis[t] != -1e9;
+}
+void mcf()
+{
+    int x = INT_MAX / 2;
+    for (int i = fr[t]; i; i = fr[e[i].no]) {
+        x = min(x, e[i].va);
+    }
+    sm += x;
+    for (int i = fr[t]; i; i = fr[e[i].no]) {
+        e[i].va -= x;
+        e[i ^ 1].va += x;
+        ans += 1LL * e[i].c * x;
+    }
+}
+int main()
+{
+    
+}
