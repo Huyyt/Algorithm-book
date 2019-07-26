0770cb37df70a52f45615c2ec29a019031550f0f
Huyyt <38221302+Huyyt@users.noreply.github.com>
2019-07-26 16:53:36 +0800

    Update Mcmf Large dumpling.cpp

--- Graph/Mcmf Large dumpling.cpp
+++ Graph/Mcmf Large dumpling.cpp
@@ -1,107 +1,131 @@
 //Mcmf LargeDumpling
 #include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+typedef int JQK;
+
 const int INF = 0x7f7f7f7f;
+
 const int MAXN = 505, MAXM = 1300;
-int Head[MAXN], cur[MAXN], lev[MAXN], to[MAXM << 1], nxt[MAXM << 1], f[MAXM << 1], mono[MAXM << 1], ed = 1, S, T;
-int pre[MAXN];
+int Head[MAXN], cur[MAXN], to[MAXM << 1], nxt[MAXM << 1], f[MAXM << 1], ed = 1;
+int S, T, MAXP, MAXF, pre[MAXN];
+JQK lev[MAXN], mono[MAXM << 1];
 bool exist[MAXN];
-void init() {
-        memset(Head, 0, sizeof(Head));
-        ed = 1;
-        S = 498;
-        T = 499;
-        return;
+void addedge(int u, int v, int cap, JQK val)
+{
+    to[++ed] = v;
+    nxt[ed] = Head[u];
+    Head[u] = ed;
+    f[ed] = cap;
+    mono[ed] = val;
+    to[++ed] = u;
+    nxt[ed] = Head[v];
+    Head[v] = ed;
+    f[ed] = 0;
+    mono[ed] = -1 * val;
+    return;
 }
-void addedge(int u, int v, int cap, int val) {
-        to[++ed] = v;
-        nxt[ed] = Head[u];
-        Head[u] = ed;
-        f[ed] = cap;
-        mono[ed] = val;
-        to[++ed] = u;
-        nxt[ed] = Head[v];
-        Head[v] = ed;
-        f[ed] = 0;
-        mono[ed] = -1 * val;
-        return;
-}
-bool BFS() {
-        int u;
-        queue<int>q;
-        memset(exist, false, sizeof(exist));
-        memset(lev, 127, sizeof(lev));
-        lev[S] = pre[S] = 0;
-        q.push(S);
-        while (q.size()) {
-                u = q.front();
-                q.pop();
-                exist[u] = false;
-                for (int i = Head[u]; i; i = nxt[i])
-                        if (f[i] && lev[u] + mono[i] < lev[to[i]]) {
-                                lev[to[i]] = lev[u] + mono[i];
-                                pre[to[i]] = i;
-                                if (!exist[to[i]]) {
-                                        exist[to[i]] = true;
-                                        q.push(to[i]);
-                                }
-                        }
-        }
-        memcpy(cur, Head, sizeof(Head));
-        return lev[T] != INF;
-}
-int DFS(int u, int maxf) {
-        if (u == T || !maxf) {
-                return maxf;
-        }
-        exist[u] = true;
-        int cnt = 0;
-        for (int &i = cur[u], tem; i; i = nxt[i])
-                if (f[i] && lev[u] + mono[i] == lev[to[i]]) {
-                        if (exist[to[i]]) {
-                                continue;
-                        }
-                        tem = DFS(to[i], min(f[i], maxf));
-                        maxf -= tem;
-                        f[i] -= tem;
-                        f[i ^ 1] += tem;
-                        cnt += tem;
-                        if (!maxf) {
-                                break;
-                        }
-                }
-        if (!cnt) {
-                lev[u] = -1 * INF;
-        }
+bool BFS()
+{
+    int u;
+    queue<int>q;
+    for (int i = 0; i <= MAXP + 1; i++) {
+        exist[i] = 0;
+        lev[i] = INF;
+    }
+    //memset(exist, false, sizeof(exist));
+    //memset(lev, 127, sizeof(lev));
+    lev[S] = pre[S] = 0;
+    q.push(S);
+    while (q.size()) {
+        u = q.front();
+        q.pop();
         exist[u] = false;
-        return cnt;
-}
-int Augment() {
-        int delta = INF;
-        for (int i = pre[T]; i; i = pre[to[i ^ 1]])
-                if (f[i] < delta) {
-                        delta = f[i];
+        for (int i = Head[u]; i; i = nxt[i])
+            if (f[i] && lev[u] + mono[i] < lev[to[i]]) {
+                lev[to[i]] = lev[u] + mono[i];
+                pre[to[i]] = i;
+                if (!exist[to[i]]) {
+                    exist[to[i]] = true;
+                    q.push(to[i]);
                 }
-        for (int i = pre[T]; i; i = pre[to[i ^ 1]]) {
-                f[i] -= delta;
-                f[i ^ 1] += delta;
+            }
+    }
+    for (int i = 0; i <= MAXP + 1; i++) {
+        cur[i] = Head[i];
+    }
+    //memcpy(cur, Head, sizeof(Head));
+    return lev[T] != INF;
+}
+JQK Augment()
+{
+    int delta = INF;
+    for (int i = pre[T]; i; i = pre[to[i ^ 1]])
+        if (f[i] < delta) {
+            delta = f[i];
         }
-        return delta * lev[T];
+    for (int i = pre[T]; i; i = pre[to[i ^ 1]]) {
+        f[i] -= delta;
+        f[i ^ 1] += delta;
+    }
+    MAXF += delta;
+    return delta * lev[T];
 }
-int MCMF() {
-        int ans = 0;
-        memset(exist, false, sizeof(exist));
-        while (BFS())
-                //ans+=DFS(S,INF)*lev[T];
-        {
-                ans += Augment();
+JQK MCMF()
+{
+    JQK ans = 0;
+    memset(exist, false, sizeof(exist));
+    while (BFS())
+        //ans+=DFS(S,INF)*lev[T];
+    {
+        ans += Augment();
+    }
+    return ans;
+}
+void init(int x123, int y123)
+{
+    MAXP = MAXF = 0;
+    memset(Head, 0, sizeof(Head));
+    ed = 1;
+    S = x123;
+    T = y123;
+    return;
+}
+int main()
+{
+
+
+    return 0;
+}
+/*int DFS(int u, int maxf)
+{
+    if (u == T || !maxf) {
+        return maxf;
+    }
+    exist[u] = true;
+    int cnt = 0;
+    for (int &i = cur[u], tem; i; i = nxt[i])
+        if (f[i] && lev[u] + mono[i] == lev[to[i]]) {
+            if (exist[to[i]]) {
+                continue;
+            }
+            tem = DFS(to[i], min(f[i], maxf));
+            maxf -= tem;
+            f[i] -= tem;
+            f[i ^ 1] += tem;
+            cnt += tem;
+            if (!maxf) {
+                break;
+            }
         }
-        return ans;
-}
-int main() {
-        init();
-        return 0;
-}
+    if (!cnt) {
+        lev[u] = -1 * INF;
+    }
+    exist[u] = false;
+    return cnt;
+}*/
+
 
 
 
