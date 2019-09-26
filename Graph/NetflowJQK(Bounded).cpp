460384f10d4cd72889b38d4942154bafd809465d
Huyyt <38221302+Huyyt@users.noreply.github.com>
2019-09-26 13:34:10 +0800

    Update NetflowJQK(Bounded).cpp

--- Graph/NetflowJQK(Bounded).cpp
+++ Graph/NetflowJQK(Bounded).cpp
@@ -2,13 +2,14 @@
 using namespace std;
 typedef long long ll;
 typedef int JQK;
+const JQK INF=1000000005;
 namespace dinic {
-        const int MAXN = 10050;
+        const int MAXN = 30050;
         const int MAXM = 100050;
         const JQK INF = 1000000050;
         int Head[MAXN], cur[MAXN], lev[MAXN], to[MAXM << 1], nxt[MAXM << 1], ed = 1;
         int S, T, SS, TT, MAXP;
-        JQK f[MAXM << 1], JQK totflow[MAXN];
+        JQK f[MAXM << 1], totflow[MAXN];
         inline void addedge(int u, int v, JQK cap) {
                 to[++ed] = v;
                 nxt[ed] = Head[u];
@@ -24,18 +25,6 @@ namespace dinic {
                 addedge(u, v, captop - caplow);
                 totflow[u] -= caplow, totflow[v] += caplow;
                 return ;
-        }
-        inline JQK pushdownflow(int x) {
-                JQK sum = 0;
-                for (int i = 1; i <= x; i++) {
-                        if (totflow > 0) {
-                                addedge(SS, i, totflow[i]);
-                                sum += totflow[i];
-                        } else if (totflow < 0) {
-                                addedge(i, TT, -totflow[i]);
-                        }
-                }
-                return sum;//sum==dinic 循环流有解
         }
         inline bool BFS() {
                 int u;
@@ -96,9 +85,13 @@ namespace dinic {
                 }
                 return ans;
         }
+        void del(int x) {
+                for (int i = Head[x]; i; i = nxt[i])
+                        f[i] = f[i ^ 1] = 0;
+        }
         void init(int S1, int T1, int S2, int T2) {
                 for (int i = 0; i <= MAXP + 1; i++) {
-                        Head[i] = 0;
+                        totflow[i] = Head[i] = 0;
                 }
                 ed = 1;
                 S = S1;
@@ -107,25 +100,41 @@ namespace dinic {
                 TT = T2;
                 return;
         }
-        JQk work() { //有源汇最大流
+        inline JQK pushdownflow(int x) {
+                JQK sum = 0;
+                for (int i = 1; i <= x; i++) {
+                        if (totflow[i] > 0) {
+                                addedge(SS, i, totflow[i]);
+                                sum += totflow[i];
+                        } else if (totflow[i] < 0) {
+                                addedge(i, TT, -totflow[i]);
+                        }
+                }
+                return sum;//sum==dinic 循环流有解
+        }
+        JQK work() { //有源汇最大流
                 JQK ans = 0;
+                JQK sum = pushdownflow(TT - 2);
                 addedge(T, S, INF);
-                pushdownflow(TT - 2);
                 swap(S, SS), swap(T, TT);
-                Dinic();//Dinic(SS,TT) 求出一个有源汇有上下界可行流.此时的流不一定最大.
+                if (sum != Dinic()) { //Dinic(SS,TT) 求出一个有源汇有上下界可行流.此时的流不一定最大.
+                        cout << -1 << endl;
+                        return 0;
+                }
                 ans = f[ed]; //可行流流量
                 f[ed] = f[ed ^ 1] = 0;
                 swap(S, SS), swap(T, TT); //Dinic(S,T)
                 ans += Dinic();//新增广出的s-t流量
+                cout << ans << endl;
                 return ans;
         }
 }
 int main() {
-        int n, m,s1,t1,s2,t2;
         int u, v;
         JQK c;
-        dinic::MAXP = n;
-        dinic::init(s1,t1,s2,t2);
+        scanf("%d %d %d", &n, &m, &k);
+        dinic::MAXP = n + m;
+        dinic::init(s1, t1, s2, t2); //S T SS TT
         dinic::work();
         return 0;
 }
